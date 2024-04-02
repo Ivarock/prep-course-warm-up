177e6b0ca6fabb695624acde6c7c1d6ce8c5970f
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-02 20:32:30 +0000

    created a function which checks for blanks

--- src/13-is-blank.ts
+++ src/13-is-blank.ts
@@ -3,6 +3,9 @@ export {};
 /**
  * Create a function called isBlank, which checks if passed string is blank or not
  */
+function isBlank(value: any) {
+    return value === null || value.trim() === '';
+}
 
 console.log(isBlank(null)); // Expected output: true
 console.log(isBlank("")); // Expected output: true

17fb82bb6813d502562c0cca5b9d1484ffaaef8a
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-02 21:58:24 +0000

    wrote a function and return statement using .split() method that converts string to array.

--- src/20-arrays.ts
+++ src/20-arrays.ts
@@ -7,5 +7,8 @@ export {};
  *  - https://www.w3schools.com/js/js_arrays.asp
  *  - https://javascript.info/array
  */
+function stringToArray(value: any) {
+    return value[0].split(' ');
+}
 
 console.log(stringToArray(["John Doe"])); // Expected output: ['John', 'Doe']

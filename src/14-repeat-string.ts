e2ff0a1a7a731a0c91922f41bba3b1d24592e36c
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-02 20:36:32 +0000

    added function parameters and return statement for expected output

--- src/14-repeat-string.ts
+++ src/14-repeat-string.ts
@@ -1,6 +1,8 @@
 export {};
 
-const repeatString = () => {};
+const repeatString = (str: string, num: number) => {
+    return str.repeat(num);
+};
 
 console.log(repeatString("a", 4)); // Expected output: 'aaaa'
 console.log(repeatString("b", 5)); // Expected output: 'bbbbb'

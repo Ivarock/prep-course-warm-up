8029ed2037dc3f46e0c1ab4b394bcf3b7c6cab95
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-05 08:51:11 +0000

    used Math.min() to find smallest numbers, and a for loop to find the biggest ones

--- src/23-min-max.ts
+++ src/23-min-max.ts
@@ -5,9 +5,19 @@ export {};
  * one of which does not use built-in Math methods.
  */
 
-const min = array => {};
+const min = (array: number[]) => {
+    return Math.min(...array);
+};
 
-const max = array => {};
+const max = (array: number[]) => {
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+};
 
 console.log(min([1, 2, 3, 4, 5])); // Expected output: 1
 console.log(min([9, -3, 6])); // Expected output: -3

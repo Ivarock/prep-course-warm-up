0727e76fb4a0ff3d176ba026b9986e87e544646d
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-05 18:35:03 +0000

    converted all to lowercase and used regExp within .match() to find all occurances of substring.

--- src/39-string-occurences.ts
+++ src/39-string-occurences.ts
@@ -1,6 +1,15 @@
 export {};
+// Add value types.
+const count = (str: string, subStr: string): number => {
+    // Convert both strings to lowercase.
+    const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
+    // Using match with regular expression to find all occurances of substring
+    const matches = lowerStr.match(new RegExp(lowerSubStr, 'g'));
+    // If there is no matches we return 0, otherwise we return the length of matches
+    return matches? matches.length : 0;
 
-const count = () => {};
+};
 
 console.log(count("The quick brown fox jumps over the lazy dog", "the")); // Expected output: 2
 console.log(count("The quick brown fox jumps over the lazy dog", "fox")); // Expected output: 1

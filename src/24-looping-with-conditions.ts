2f6aada04253cbd0dbb71b3f3357586088fcc511
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-05 08:59:58 +0000

    used .forEach() method to find needed value and push it to the new array.

--- src/24-looping-with-conditions.ts
+++ src/24-looping-with-conditions.ts
@@ -1,5 +1,13 @@
 export {};
 
-function onlyTheAces(arr) {}
+function onlyTheAces(arr: string[], value: string) {
+    let aces: string [] = [];
+    arr.forEach(element => {
+        if(element === value) {
+            aces.push(element);
+        }
+    })
+    return aces;
+}
 
-console.log(onlyTheAces(["Ace", "King", "Queen", "Jack", "Ace"])); // Expected result: ['Ace', 'Ace']
+console.log(onlyTheAces(["Ace", "King", "Queen", "Jack", "Ace"],"Ace")); // Expected result: ['Ace', 'Ace']

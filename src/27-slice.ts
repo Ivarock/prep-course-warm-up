a3b773b02b7dd36198a75287a65ee24a0b06b584
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-05 09:55:49 +0000

    created variables storing start and end indexes in a function, and called them in return statement.

--- src/27-slice.ts
+++ src/27-slice.ts
@@ -4,6 +4,12 @@ export {};
  * Write function first5 which returns first five elements from the array
  */
 
+function first5(arr: number[]): number[] {
+    const startIndex = 0;
+    const endIndex = 5;
+    return arr.slice(startIndex, endIndex);
+}
+
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8]; // You are NOT allowed to edit this line
 const first5Numbers = first5(numbers); // You are NOT allowed to edit this line
 

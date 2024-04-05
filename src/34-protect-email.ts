8f6ff5beb8cc31b2cc073faf2c98b8aca6e0e44b
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-05 16:39:30 +0000

    used destructuring for emails and if/ else if statements to filter different email lengths and get outputs accordingly

--- src/34-protect-email.ts
+++ src/34-protect-email.ts
@@ -4,6 +4,18 @@ export {};
  * Create a function protectEmail which hides some symbols of the email
  */
 
+const protectEmail = (email: string): string => {
+    const [localPart, domainPart] = email.split('@');
+    let hiddenLocalPart = '';
+        if (localPart.length >= 7) {
+        hiddenLocalPart = `${localPart.slice(0, 3)}...`;
+    } else if (localPart.length >= 5) {
+        hiddenLocalPart = `${localPart.slice(0, 2)}...`;
+    } else {
+        hiddenLocalPart = '...'
+    }
+    return `${hiddenLocalPart}@${domainPart}`;
+}
 console.log(protectEmail("secret123@codelex.io")); // Expected result: sec...@codelex.io
 console.log(protectEmail("example@example.com")); // Expected result: exa...@example.com
 console.log(protectEmail('12345@example.com')); // Expected result: 12...@example.com

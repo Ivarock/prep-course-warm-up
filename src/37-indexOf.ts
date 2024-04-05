e25338f5e6ab97fe71b83a25466d135174d61677
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-05 17:20:59 +0000

    wrote some comments

--- src/37-indexOf.ts
+++ src/37-indexOf.ts
@@ -8,7 +8,17 @@ export {};
  */
 
 // You are allowed to edit only this function
-function remove(arr, valueToRemove) {}
+function remove(arr: (string | number)[], valueToRemove: string | number): (string | number)[] {
+  // Finding the index of value to be removed
+  const index = arr.indexOf(valueToRemove);
+  if (index === -1) {
+    //  If the index is not found we return the copy of array
+    return arr.slice();
+  } else {
+    // If the index is found, we return the part of array before index and concatenate it with part after the index
+    return arr.slice(0, index).concat(arr.slice(index + 1));
+  }
+}
 
 const numbers = [1, 2, 3];
 const names = ["John", "Alice", "Ellen"];
